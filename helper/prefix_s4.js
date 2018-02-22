24d5b1ae4128587d943c17169dffb3af6bb1bc55
Dingyj3178 <ll3178292@gmail.com>
2018-02-22 17:13:46 +0900

    bug fix

--- helper/prefix_s4.js
+++ helper/prefix_s4.js
@@ -27,7 +27,7 @@ module.exports = (filename_tb,filename_def) =>{
                     if(find_def_key(sheet_s4_def,sheet_s4[utils.encode_cell({c:c_s4, r:0})].v) === conf.extractor_type.DATS){
                         for(let r_s4 = decodeRange_s4.s.r+1; r_s4 <= decodeRange_s4.e.r; r_s4++){
                             book_s4_s.Sheets['Sheet1'][utils.encode_cell({c:c_s4, r:r_s4})].z = 'yyyymmdd';
-                            book_s4_s.Sheets['Sheet1'][utils.encode_cell({c:c_s4, r:r_s4})].t = 's';
+                            // book_s4_s.Sheets['Sheet1'][utils.encode_cell({c:c_s4, r:r_s4})].t = 's';
                             delete book_s4_s.Sheets['Sheet1'][utils.encode_cell({c:c_s4, r:r_s4})].s;
                         
                         // console.log(book_s4_s.Sheets['Sheet1'][utils.encode_cell({c:c_s4, r:r_s4})]);
@@ -91,6 +91,18 @@ module.exports = (filename_tb,filename_def) =>{
                 xlsx_style.writeFile(book_s4_s, path.join(conf.root,'/tables_s4_prefix',filename_tb));
                 callback(null, null); 
             }
+            // function (callback){
+            //     const filePath_tb = path.join(conf.root,'/tables_s4_prefix',filename_tb);
+            //     const book_s4 = xlsx.readFile(filePath_tb);
+            //     const book_s4_s = xlsx_style.readFile(filePath_tb,{cellStyles: true,cellDates:true});
+            //     const sheetNames_s4 = book_s4.SheetNames;
+            //     const sheet_s4 = book_s4.Sheets[sheetNames_s4[0]];
+            //     const decodeRange_s4 = utils.decode_range(sheet_s4['!ref']);
+            //     decodeRange_s4.e.c = decodeRange_s4.e.c -1;
+            //     book_s4_s.Sheets['Sheet1'][utils.decode_range(book_s4_s.Sheets['Sheet1']['!ref'])] = decodeRange_s4;
+            //     xlsx_style.writeFile(book_s4_s, path.join(conf.root,'/tables_s4_prefix',filename_tb));
+            //     callback(null, null); 
+            // }
         ]
         // function (err, results) {
         //     // results是返回值的数组
